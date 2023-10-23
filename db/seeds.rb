c0079ef3b36211d62158f8b140dff370b3229e20
Olena Reikh <olena.reikh@gmail.com>
2023-10-23 16:29:20 -0500

    Add Faker to seed Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+Product.delete_all
+
+676.times do
+  Product.create(
+    title:          Faker::Commerce.product_name,
+    price:          Faker::Commerce.price(range: 10.0..1000.0, as_string: false),
+    stock_quantity: Faker::Number.between(from: 1, to: 1000)
+  )
+end
+
+puts "676 fake products have been created."
