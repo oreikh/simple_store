52e1fa16b5f241aa0a48ff4209e10021bf6cb47c
Olena Reikh <olena.reikh@gmail.com>
2023-10-23 17:04:31 -0500

    Get Products from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,37 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "faker"
-Product.delete_all
+require "csv"
 
-676.times do
-  Product.create(
-    title:          Faker::Commerce.product_name,
-    price:          Faker::Commerce.price(range: 10.0..1000.0, as_string: false),
-    stock_quantity: Faker::Number.between(from: 1, to: 1000)
+Product.destroy_all
+Category.destroy_all
+
+# 676.times do
+#   Product.create(
+#     title:          Faker::Commerce.product_name,
+#     price:          Faker::Commerce.price(range: 10.0..1000.0, as_string: false),
+#     stock_quantity: Faker::Number.between(from: 1, to: 1000)
+#   )
+# end
+
+csv_file = Rails.root.join("db/products.csv")
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  title = product["name"]
+  description = product["description"]
+  price = product["price"]
+  stock_quantity = product["stock quantity"]
+  category_name = product["category"]
+  category = Category.find_or_create_by(name: category_name)
+
+  # Create the product
+  Product.create!(
+    title:,
+    description:,
+    price:,
+    stock_quantity:,
+    category: # Associate the product with the category
   )
 end
-
-puts "676 fake products have been created."
