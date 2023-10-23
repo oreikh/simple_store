227d85694616f4515762305d04fc8646019860c8
Olena Reikh <olena.reikh@gmail.com>
2023-10-23 16:47:58 -0500

    Add Categoty reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_211821) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_214619) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_211821) do
     t.integer "stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
