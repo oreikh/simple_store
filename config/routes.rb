fc28f13a31f34a22dd9bc701cb6d39dca59ccebb
Olena Reikh <olena.reikh@gmail.com>
2023-10-23 16:36:14 -0500

    Update Product index controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  get "/products", to: "products#index"
+  get "products/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
